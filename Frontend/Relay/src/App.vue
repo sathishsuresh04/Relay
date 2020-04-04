88ceabf8d3366669fdf27c25a8415abffedf5da5
Matilda Höglund <48841764+jensengbg-matilda-hoglund@users.noreply.github.com>
2020-04-04 17:07:34 +0200

    upd with vuex

---
+++ Frontend/Relay/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+// google font
+@import url('https://fonts.googleapis.com/css?family=Lato&display=swap');
+
+</style>
---
+++ Frontend/Relay/src/router/index.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+  const routes = [
+  {
+    path: '/',
+    name: 'Landing',
+    component: () => import('../views/Landing.vue')
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import('../views/About.vue')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: () => import('../views/Register.vue')
+  },
+  {
+    path: '/match',
+    name: 'Match Profile',
+    component: () => import('../views/Match.vue')
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: () => import('../views/Contact.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
