6326757c9086c64643bfca88e6a654ee1e051a27
Matilda Höglund <48841764+jensengbg-matilda-hoglund@users.noreply.github.com>
2020-04-04 19:09:39 +0200

    some basic changes

---
+++ Frontend/Relay/src/views/Register.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="wrapper">
+<ExitBtn />
+    </div>
+</template>
+
+<script>
+import ExitBtn from "../components/buttons/exitBtn";
+
+export default {
+    components: {
+        ExitBtn
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/variables.scss";
+@import "../assets/scss/buttons.scss";
+
+.wrapper {
+    background: $darkBlue;
+    width: 100vw;
+    height: 100vh;
+}
+
+</style>
